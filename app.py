63e96e532eb3b87514f31610f8d69f927508c9e5
Matthieu Hédoin <matthieu@ewill.fr>
2021-05-18 10:11:07 +0200

    Add routes for templates

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index/index.html')
+
+@app.route('/artistes')
+def index():
+    return render_template('artistes/artistes.html')
+
+@app.route('/albums')
+def index():
+    return render_template('albums/albums.html')
+
+@app.route('/employes')
+def index():
+    return render_template('employes/employes.html')
+
+@app.route('/genres')
+def index():
+    return render_template('genres/genres.html')
