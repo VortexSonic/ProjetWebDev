bc33a5001611aff40ad190400c07200d28e74cd9
Matthieu Hédoin <matthieu@ewill.fr>
2021-05-18 10:39:56 +0200

    fix

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ def index():
 
 @app.route('/artistes')
 def artistes():
-<<<<<<< HEAD
-    return render_template('index/artistes.html')
-=======
     return render_template('html/artistes.html')
 
 @app.route('/albums')
@@ -24,4 +21,3 @@ def employes():
 @app.route('/genres')
 def genres():
     return render_template('html/genres.html')
->>>>>>> 82ecd7d0c2a2d264ae6c0713aab7fa7d9648d9fe
