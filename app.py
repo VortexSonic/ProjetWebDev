b256388184852f52f08e86b558810a7e0c1ee399
VortexSonic <84374279+VortexSonic@users.noreply.github.com>
2021-05-18 09:57:59 +0200

    Update du app.py
    Ajout de la route pour l'accès à la page "Artiste"

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index/index.html')
+
+@app.route('/artistes')
+def artistes():
+    return render_template('index/artistes.html')
