b697faa470853ecbebdbc94ea8418db2e1c16f58
Matthieu Hédoin <hedoinm@gmail.com>
2021-06-05 22:00:43 +0200

    test
    - Ajout du dossier templates
    - Test avec "flask run" qui ne fonctionnait pas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-import requests
+
 from flask import Flask, render_template
 from pprint import pprint
 
@@ -8,33 +8,33 @@ app = Flask(__name__)
 
 @app.route('/albums')
 def albums():
-    return render_template('html/albums.html')
+    return render_template('albums.html')
 
 @app.route('/artistes')
 def artistes():
-    return render_template('html/artistes.html')
+    return render_template('artistes.html')
     
 @app.route('/clients')
 def clients():
-    return render_template('html/clients.html')
+    return render_template('clients.html')
 
 @app.route('/commandes')
 def commandes():
-    return render_template('html/commandes.html')
+    return render_template('commandes.html')
 
 @app.route('/employes')
 def employes():
-    return render_template('html/employes.html')
+    return render_template('employes.html')
 
 @app.route('/genres')
 def genres():
-    return render_template('html/genres.html')
+    return render_template('genres.html')
 
 @app.route('/')
 def index():
-    return render_template('html/index.html')
+    return render_template('index.html')
 
 @app.route('/infos')
 def infos():
-    return render_template('html/infos.html')
+    return render_template('infos.html')
 
